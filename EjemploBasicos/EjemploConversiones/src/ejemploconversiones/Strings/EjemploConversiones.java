33dd993076ed3eef0eaa28a96f4699b3fdc1fb26
Alejandra Pinto <copinto-@student.42.us.org>
2020-05-29 23:59:07 +0200

    Add files via upload

---
+++ EjemploBasicos/EjemploConversiones/src/ejemploconversiones/Strings/EjemploConversiones.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploconversiones;
+
+/**
+ *
+ * @author Arkanabytes
+ */
+public class EjemploConversiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int a =5;
+        int b =7;
+        double c = 0.5;
+        c= a;
+        a = (int)c;
+        String s ="7";
+        b = Integer.parseInt(s);
+      
+        c = Double.parseDouble (S);
+    }
+    }
+    
+}
