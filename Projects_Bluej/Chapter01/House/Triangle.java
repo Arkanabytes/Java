937eef6d1291ad1ce24668508eb6a29a15383937
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-06-02 16:04:04 +0200

    Java

--- Projects_Bluej/Chapter01/House/Triangle.java
+++ Projects_Bluej/Chapter01/House/Triangle.java
@@ -1,11 +1,6 @@
 import java.awt.*;
 
-/**
- * A triangle that can be manipulated and that draws itself on a canvas.
- * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
- */
+/*A triangle that can be manipulated and that draws itself on a canvas.*/
 
 public class Triangle
 {
