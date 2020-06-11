79c6dc99bfffcd2c4f74e7fbc339effd9be9d7e3
Alejandra Pinto <copinto-@student.42.us.org>
2020-06-11 21:57:13 +0200

    Java

---
+++ Projects_Bluej/chapter04/auction/club/ClubDemo.java
@@ -0,0 +1,36 @@
+
+/**
+ * Provide a demonstration of the Club and Membership
+ * classes.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClubDemo
+{
+    // instance variables - replace the example below with your own
+    private Club club;
+
+    /**
+     * Constructor for objects of class ClubDemo
+     */
+    public ClubDemo()
+    {
+        club = new Club();
+    }
+
+    /**
+     * Add some members to the club, and then
+     * show how many there are.
+     * Further example calls could be added if more functionality
+     * is added to the Club class.
+     */
+    public void demo()
+    {
+        club.join(new Membership("Arkana", 2, 2002));
+        club.join(new Membership("Alejandra", 1, 2002));
+        System.out.println("The club has " +
+                           club.numberOfMembers() +
+                           " members.");
+    }
+}
