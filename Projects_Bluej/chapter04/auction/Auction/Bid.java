5187a09f41e48fa482b88c5e9a4f6ea8feaede68
Alejandra Pinto <copinto-@student.42.us.org>
2020-06-11 21:53:40 +0200

    Java

--- Projects_Bluej/chapter04/auction/Auction/Bid.java
+++ Projects_Bluej/chapter04/auction/Auction/Bid.java
@@ -1,6 +1,10 @@
-/*A class that models an auction bid.
- * It contains a reference to the Person bidding and the amount bid.*/
-
+/**
+ * A class that models an auction bid.
+ * It contains a reference to the Person bidding and the amount bid.
+ * 
+ * @author Consuelo Pinto
+ * @version 2016.02.29
+ */
 public class Bid
 {
     // The person making the bid.
@@ -9,23 +13,28 @@ public class Bid
     // the long type has been used.
     private final long value;
 
-    /*Create a bid.
+    /**
+     * Create a bid.
      * @param bidder Who is bidding for the lot.
-     * @param value The value of the bid.*/
-    
+     * @param value The value of the bid.
+     */
     public Bid(Person bidder, long value)
     {
         this.bidder = bidder;
         this.value = value;
     }
 
-    /*@return The bidder.*/
+    /**
+     * @return The bidder.
+     */
     public Person getBidder()
     {
         return bidder;
     }
 
-    /**@return The value of the bid.*/
+    /**
+     * @return The value of the bid.
+     */
     public long getValue()
     {
         return value;
