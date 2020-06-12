e9577b4e1d78efb471079b690a7d748484f7b476
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-06-12 23:06:32 +0200

    Java

--- Projects_Bluej/chapter04/auction/products/Product.java
+++ Projects_Bluej/chapter04/auction/products/Product.java
@@ -1,9 +1,4 @@
-/**
- * Model some details of a product sold by a company.
- * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
- */
+/*Model some details of a product sold by a company.*/
 public class Product
 {
     // An identifying number for this product.
