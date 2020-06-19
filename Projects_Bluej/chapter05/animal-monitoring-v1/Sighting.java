133ee519c6802584ff84b8cd16766633eed1ccc3
Alejandra Pinto <copinto-@student.42.us.org>
2020-06-19 22:41:48 +0200

    .

--- Projects_Bluej/chapter05/animal-monitoring-v1/Sighting.java
+++ Projects_Bluej/chapter05/animal-monitoring-v1/Sighting.java
@@ -1,4 +1,9 @@
-/*Details of a sighting of a type of animal by an individual spotter.*/
+/**
+ * Details of a sighting of a type of animal by an individual spotter.
+ * 
+ * @author Consuelo Pinto
+ * @version 2016.02.29
+ */
 public class Sighting
 {
     // The animal spotted.
@@ -12,12 +17,14 @@ public class Sighting
     // The reporting period.
     private final int period;
     
-    /*Create a record of a sighting of a particular type of animal.
+    /**
+     * Create a record of a sighting of a particular type of animal.
      * @param animal The animal spotted.
      * @param spotter The ID of the spotter.
      * @param count How many were seen (>= 0).
      * @param area The ID of the area in which they were seen.
-     * @param period The reporting period.*/
+     * @param period The reporting period.
+     */
     public Sighting(String animal, int spotter, int count, int area, int period)
     {
         this.animal = animal;
@@ -27,26 +34,37 @@ public class Sighting
         this.period = period;
     }
 
-    /*Return the type of animal spotted. @return The animal type.*/
+    /**
+     * Return the type of animal spotted.
+     * @return The animal type.
+     */
     public String getAnimal() 
     {
         return animal;
     }
 
-    /*Return the ID of the spotter.
-     * @return The spotter's ID.*/
+    /**
+     * Return the ID of the spotter.
+     * @return The spotter's ID.
+     */
     public int getSpotter() 
     {
         return spotter;
     }
 
-    /*Return how many were spotted. @return The number seen. */
+    /**
+     * Return how many were spotted.
+     * @return The number seen.
+     */
     public int getCount() 
     {
         return count;
     }
 
-    /*Return the ID of the area in which they were seen. @return Where they were seen. */
+    /**
+     * Return the ID of the area in which they were seen.
+     * @return Where they were seen.
+     */
     public int getArea() 
     {
         return area;
@@ -61,7 +79,8 @@ public class Sighting
         return period;
     }
 
-    /*Return a string containing details of the animal, the number seen,
+    /**
+     * Return a string containing details of the animal, the number seen,
      * where they were seen, who spotted them and when.
      * @return A string giving details of the sighting.
      */
