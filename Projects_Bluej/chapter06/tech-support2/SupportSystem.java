2c88d17b947ca543a34da968d46a734977776172
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-06-19 22:52:55 +0200

    .

--- Projects_Bluej/chapter06/tech-support2/SupportSystem.java
+++ Projects_Bluej/chapter06/tech-support2/SupportSystem.java
@@ -1,24 +1,17 @@
-/**
- * This class implements a technical support system. It is the top level class 
+/*This class implements a technical support system. It is the top level class 
  * in this project. The support system communicates via text input/output 
  * in the text terminal.
  * 
  * This class uses an object of class InputReader to read input from the user,
  * and an object of class Responder to generate responses. It contains a loop
  * that repeatedly reads input and generates output until the users wants to 
- * leave.
- * 
- * @author     Michael Kölling and David J. Barnes
- * @version    0.2 (2016.02.29)
- */
+ * leave. */
 public class SupportSystem
 {
     private InputReader reader;
     private Responder responder;
     
-    /**
-     * Creates a technical support system.
-     */
+    /*Creates a technical support system.*/
     public SupportSystem()
     {
         reader = new InputReader();
