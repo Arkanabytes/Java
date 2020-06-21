e6b3bf8e7e6440db7de8fa43db9bab9898bf44fa
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-06-21 20:48:11 +0200

    .

--- Projects_Bluej/chapter06/tech-support2/Responder.java
+++ Projects_Bluej/chapter06/tech-support2/Responder.java
@@ -29,8 +29,7 @@ public class Responder
         return responses.get(index);
     }
 
-    /**
-     * Build up a list of default responses from which we can pick one
+    /*Build up a list of default responses from which we can pick one
      * if we don't know what else to say.*/
     private void fillResponses()
     {
