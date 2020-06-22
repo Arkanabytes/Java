bc215f831832917465f1a87498b68625abb37b8e
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-06-22 22:24:10 +0200

    .

--- Projects_Bluej/chapter07/automaton-v2/Automaton.java
+++ Projects_Bluej/chapter07/automaton-v2/Automaton.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-
 /*Model a 1D elementary cellular automaton.*/
 public class Automaton
 {
@@ -8,8 +7,7 @@ public class Automaton
     // The state of the cells.
     private int[] state;
     
-    /*Create a 1D automaton consisting of the given number of cells.
-     * @param numberOfCells The number of cells in the automaton.*/
+    /*Create a 1D automaton consisting of the given number of cells. @param numberOfCells The number of cells in the automaton.*/
     public Automaton(int numberOfCells)
     {
         this.numberOfCells = numberOfCells;
