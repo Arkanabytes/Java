969ec6b04eb24184148a1a0ac4bb19434eacb11a
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-31 23:58:58 +0200

    .

--- Projects_Bluej/chapter05/animal-monitoring-v1/AnimalMonitor.java
+++ Projects_Bluej/chapter05/animal-monitoring-v1/AnimalMonitor.java
@@ -30,8 +30,7 @@ public class AnimalMonitor
     }
     
     /*Print the details of all the sightings of the given animal.
-     * @param animal The type of animal.
-     */
+     * @param animal The type of animal.*/
     public void printSightingsOf(String animal)
     {
         for(Sighting record : sightings) {
