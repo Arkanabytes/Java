830a0d10635690f31e00dbb73878f4887e4d5838
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-02 22:59:17 +0200

    .

--- Projects_Bluej/chapter02/book-exercise/Book.java
+++ Projects_Bluej/chapter02/book-exercise/Book.java
@@ -1,21 +1,13 @@
-/**
- * A class that maintains information on a book.
+/*A class that maintains information on a book.
  * This might form part of a larger application such
- * as a library system, for instance.
- *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
- */
+ * as a library system, for instance.*/
 class Book
 {
     // The fields.
     private String author;
     private String title;
 
-    /**
-     * Set the author and title fields when this object
-     * is constructed.
-     */
+    /*Set the author and title fields when this object is constructed.*/
     public Book(String bookAuthor, String bookTitle)
     {
         author = bookAuthor;
