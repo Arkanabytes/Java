67f0e034382a5c57d2fb7e5e06a8caae7eb8e042
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-02 22:59:56 +0200

    .

--- Projects_Bluej/chapter02/naive-ticket-machine/TicketMachine.java
+++ Projects_Bluej/chapter02/naive-ticket-machine/TicketMachine.java
@@ -1,14 +1,10 @@
-/**
- * TicketMachine models a naive ticket machine that issues
+/*TicketMachine models a naive ticket machine that issues
  * flat-fare tickets.
  * The price of a ticket is specified via the constructor.
  * It is a naive machine in the sense that it trusts its users
  * to insert enough money before trying to print a ticket.
- * It also assumes that users enter sensible amounts.
- *
- * @author David J. Barnes and Michael Kölling
- * @version 2016.02.29
- */
+ * It also assumes that users enter sensible amounts. */
+
 public class TicketMachine
 {
     // The price of a ticket from this machine.
