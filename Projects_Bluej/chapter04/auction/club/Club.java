2ce271890eb1bca2cdc9a8060d0aa5d61f522af2
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-05 22:12:05 +0200

    .

--- Projects_Bluej/chapter04/auction/club/Club.java
+++ Projects_Bluej/chapter04/auction/club/Club.java
@@ -3,17 +3,14 @@ public class Club
 {
     // Define any necessary fields here ...
     
-    /**
-     * Constructor for objects of class Club
-     */
+    /*Constructor for objects of class Club*/
     public Club()
     {
         // Initialise any fields here ...
         
     }
 
-    /**
-     * Add a new member to the club's list of members.
+    /*Add a new member to the club's list of members.
      * @param member The member object to be added.
      */
     public void join(Membership member)
