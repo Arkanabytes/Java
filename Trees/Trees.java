7104a168203c91b28e837dbe9fa829dd2377058f
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-09 23:41:34 +0200

    .

--- Trees/Trees.java
+++ Trees/Trees.java
@@ -1,4 +1,5 @@
 /*Arkanabytes*/
+
     public static GTreeIF<Integer> imparDel (GTreeIF<Integer> gTree){
         for (int n=1; n<=gTree.getNumChildren (); n++){
             if(gTree.getChild(n).getRoot()%2 ==1){
@@ -8,5 +9,3 @@
         }
         return gTree;
     }
-    
-
