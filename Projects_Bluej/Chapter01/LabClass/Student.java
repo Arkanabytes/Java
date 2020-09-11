ba6f4f5cea2d944ad457c154dad5225cbc002e44
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-11 23:31:57 +0200

    .

--- Projects_Bluej/Chapter01/LabClass/Student.java
+++ Projects_Bluej/Chapter01/LabClass/Student.java
@@ -1,7 +1,6 @@
 /*Arkanabytes*/
 
 /*The Student class represents a student in a student administration system. It holds the student details relevant in our context.*/
-
 public class Student
 {
     // the student's full name
