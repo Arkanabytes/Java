2617a2c37bcb08931d289b98457bdf877d716ee6
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-23 15:26:09 +0200

    .

--- Projects_Bluej/chapter03/clock-display/NumberDisplay.java
+++ Projects_Bluej/chapter03/clock-display/NumberDisplay.java
@@ -21,10 +21,7 @@ public class NumberDisplay
         return value;
     }
 
-    /**
-     * Return the display value (that is, the current value as a two-digit
-     * String. If the value is less than ten, it will be padded with a leading
-     * zero).*/
+    /*Return the display value (that is, the current value as a two-digit String. If the value is less than ten, it will be padded with a leading zero).*/
     public String getDisplayValue()
     {
         if(value < 10) {
