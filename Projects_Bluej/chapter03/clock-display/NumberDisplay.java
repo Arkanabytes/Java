377ec08433728f0cf297f8334c0164f28bccd2df
Alejandra Pinto <copinto-@student.42.us.org>
2020-09-23 15:18:45 +0200

    .

--- Projects_Bluej/chapter03/clock-display/NumberDisplay.java
+++ Projects_Bluej/chapter03/clock-display/NumberDisplay.java
@@ -1,13 +1,7 @@
 
-/*The NumberDisplay class represents a digital number display that can hold
- * values from zero to a given limit. The limit can be specified when creating
- * the display. The values range from zero (inclusive) to limit-1. If used,
- * for example, for the seconds on a digital clock, the limit would be 60, 
- * resulting in display values from 0 to 59. When incremented, the display 
- * automatically rolls over to zero when reaching the limit.
- * 
- * @author Consuelo Pinto
- * @version 2016.02.29*/
+/*The NumberDisplay class represents a digital number display that can hold values from zero to a given limit. The limit can be specified when creating
+ * the display. The values range from zero (inclusive) to limit-1. If used,for example, for the seconds on a digital clock, the limit would be 60, 
+ * resulting in display values from 0 to 59. When incremented, the display automatically rolls over to zero when reaching the limit.*/
 public class NumberDisplay
 {
     private int limit;
